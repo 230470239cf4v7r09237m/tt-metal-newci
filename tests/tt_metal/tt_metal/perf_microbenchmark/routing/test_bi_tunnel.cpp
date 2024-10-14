tenstorrent/tt-metal

#11208: Make Program API type opaque

tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_bi_tunnel.cpp
@@ -8,6 +8,7 @@
 #include "tt_metal/impl/dispatch/cq_commands.hpp"
 #include "tt_metal/hostdevcommon/common_runtime_address_map.h"
 #include "tt_metal/impl/dispatch/kernels/packet_queue_ctrl.hpp"
+#include "tt_metal/impl/program/program_pool.hpp"
 #include "kernels/traffic_gen_test.hpp"
 #include "tt_metal/impl/device/device.hpp"
 
@@ -173,8 +174,8 @@ int main(int argc, char **argv) {
         //std::cout<<"Left Tunneler = "<<tunneler_logical_core.str()<<std::endl;
         //std::cout<<"Right Tunneler = "<<r_tunneler_logical_core.str()<<std::endl;
 
-        tt_metal::Program program = tt_metal::CreateProgram();
-        tt_metal::Program program_r = tt_metal::CreateProgram();
+        auto program = tt_metal::CreateProgram();
+        auto program_r = tt_metal::CreateProgram();
 
         CoreCoord mux_core = {mux_x, mux_y};
         CoreCoord mux_phys_core = device->worker_core_from_logical_core(mux_core);
@@ -510,11 +511,14 @@ int main(int argc, char **argv) {
 
         log_info(LogTest, "Starting test...");
 
+        auto program_ptr = tt::tt_metal::ProgramPool::instance().get_program(program);
+        auto program_r_ptr = tt::tt_metal::ProgramPool::instance().get_program(program_r);
+
         auto start = std::chrono::system_clock::now();
-        tt_metal::detail::LaunchProgram(device, program, false);
-        tt_metal::detail::LaunchProgram(device_r, program_r, false);
-        tt_metal::detail::WaitProgramDone(device, program);
-        tt_metal::detail::WaitProgramDone(device_r, program_r);
+        tt_metal::detail::LaunchProgram(device, *program_ptr, false);
+        tt_metal::detail::LaunchProgram(device_r, *program_r_ptr, false);
+        tt_metal::detail::WaitProgramDone(device, *program_ptr);
+        tt_metal::detail::WaitProgramDone(device_r, *program_r_ptr);
         auto end = std::chrono::system_clock::now();
 
         std::chrono::duration<double> elapsed_seconds = (end-start);

tests/tt_metal/tt_metal/test_matmul_multi_core_single_dram.cpp
@@ -3,14 +3,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <algorithm>
-#include <functional>
-#include <random>
 
 #include "tt_metal/host_api.hpp"
 #include "tt_metal/detail/tt_metal.hpp"
 #include "common/bfloat16.hpp"
 #include "tt_metal/test_utils/deprecated/tensor.hpp"
 #include "test_tiles.hpp"
+#include "tt_metal/impl/program/program_pool.hpp"
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // TODO: explain what test does
@@ -152,8 +151,8 @@ std::vector<bfloat16> select_columns(std::vector<bfloat16> data, int M, int K, i
     return result;
 }
 
-std::tuple<tt_metal::Program, tt_metal::KernelHandle , tt_metal::KernelHandle> create_program(tt_metal::Device *device, int num_cores_r, int num_cores_c, int per_core_M, int per_core_N, int K, int in0_block_w, int out_subblock_h, int out_subblock_w) {
-    tt_metal::Program program = tt_metal::CreateProgram();
+std::tuple<tt_metal::ProgramHandle, tt_metal::KernelHandle , tt_metal::KernelHandle> create_program(tt_metal::Device *device, int num_cores_r, int num_cores_c, int per_core_M, int per_core_N, int K, int in0_block_w, int out_subblock_h, int out_subblock_w) {
+    auto program = tt_metal::CreateProgram();
     uint32_t single_tile_size = 2 * 1024;
     uint32_t in0_block_tiles = per_core_M * in0_block_w;
     uint32_t in0_CB_size = in0_block_tiles * 2 * single_tile_size; // double buffer
@@ -249,7 +248,7 @@ std::tuple<tt_metal::Program, tt_metal::KernelHandle , tt_metal::KernelHandle> c
         tt_metal::ComputeConfig{.compile_args = compute_kernel_args}
     );
 
-    return {std::move(program), mm_reader_kernel, unary_writer_kernel};
+    return {program, mm_reader_kernel, unary_writer_kernel};
 }
 
 std::vector<bfloat16> get_row_slice(std::vector<bfloat16> data, int total_row_slices, int row_slice_index, int rows, int cols) {
@@ -395,7 +394,9 @@ int main(int argc, char **argv) {
 
         log_info(LogTest, "Running Matmul {} core test", num_cores_c * num_cores_r);
 
-        tt_metal::detail::LaunchProgram(device, program);
+        auto* program_ptr = tt::tt_metal::ProgramPool::instance().get_program(program);
+        tt_metal::detail::LaunchProgram(device, *program_ptr);
+
         log_info(LogTest, "Matmul test done");
         log_info(LogTest, "Gathering data back from dram and checking against golden");
         for(int i = 0; i < num_cores_r; i++) {

tests/tt_metal/tt_metal/unit_tests/basic/device.cpp
@@ -4,18 +4,14 @@
 
 #include <gtest/gtest.h>
 
-#include <algorithm>
-#include <functional>
-#include <random>
-
 #include "tests/tt_metal/tt_metal/unit_tests/common/basic_fixture.hpp"
 #include "tests/tt_metal/tt_metal/unit_tests/common/device_fixture.hpp"
 #include "tt_metal/detail/tt_metal.hpp"
 #include "tt_metal/host_api.hpp"
 #include "tt_metal/hostdevcommon/common_runtime_address_map.h"  // FIXME: Should remove dependency on this
 #include "tt_metal/test_utils/env_vars.hpp"
-#include "tt_metal/test_utils/print_helpers.hpp"
 #include "tt_metal/test_utils/stimulus.hpp"
+#include "tt_metal/impl/program/program_pool.hpp"
 
 using namespace tt;
 using namespace tt::test_utils;
@@ -230,7 +226,7 @@ TEST_F(DeviceFixture, ValidateKernelDoesNotTargetHarvestedCores) {
             tt_metal::detail::WriteToDeviceL1(this->devices_.at(id), logical_core, write_address, host_input);
         }
 
-        tt_metal::Program program = tt_metal::CreateProgram();
+        auto program = tt_metal::CreateProgram();
         string kernel_name = "tests/tt_metal/tt_metal/test_kernels/misc/ping_legal_l1s.cpp";
         CoreCoord logical_target_core(0, 0);
         uint32_t intermediate_l1_addr = devices_.at(id)->get_base_allocator_addr(HalMemType::L1);
@@ -244,7 +240,8 @@ TEST_F(DeviceFixture, ValidateKernelDoesNotTargetHarvestedCores) {
                 .noc = tt_metal::NOC::NOC_0,
                 .compile_args = {l1_address, intermediate_l1_addr, size_bytes}});
 
-        tt_metal::detail::LaunchProgram(this->devices_.at(id), program);
+        auto* program_ptr = tt::tt_metal::ProgramPool::instance().get_program(program);
+        tt_metal::detail::LaunchProgram(this->devices_.at(id), *program_ptr);
 
         std::vector<uint32_t> output;
         for (uint32_t bank_id = 0; bank_id < num_l1_banks; bank_id++) {
@@ -282,7 +279,7 @@ TEST_F(DeviceFixture, TestDeviceToHostMemChannelAssignment) {
 
 // Test to ensure writing from 16B aligned L1 address to 16B aligned PCIe address works
 TEST_F(DeviceFixture, TestL1ToPCIeAt16BAlignedAddress) {
-    tt_metal::Program program = tt_metal::CreateProgram();
+    auto program = tt_metal::CreateProgram();
     Device *device = this->devices_.at(0);
     EXPECT_TRUE(device->is_mmio_capable());
     CoreCoord logical_core(0, 0);
@@ -310,7 +307,8 @@ TEST_F(DeviceFixture, TestL1ToPCIeAt16BAlignedAddress) {
         }
     );
 
-    tt_metal::detail::LaunchProgram(device, program);
+    auto* program_ptr = tt::tt_metal::ProgramPool::instance().get_program(program);
+    tt_metal::detail::LaunchProgram(device, *program_ptr);
 
     std::vector<uint32_t> result(size_bytes/sizeof(uint32_t));
     chip_id_t mmio_device_id = tt::Cluster::instance().get_associated_mmio_device(device->id());

tests/tt_metal/tt_metal/unit_tests_common/dprint/test_eth_cores.cpp
@@ -44,7 +44,7 @@ static void RunTest(DPrintFixture* fixture, Device* device, bool active) {
     }
     for (const auto& core : test_cores) {
         // Set up program and command queue
-        Program program = Program();
+        auto program = CreateProgram();
 
         // Create the kernel
         // TODO: When #6424 is fixed combine these kernels again.

tests/tt_metal/tt_metal/unit_tests_common/dprint/test_mute_device.cpp
@@ -43,7 +43,7 @@ SLICE:
 static void RunTest(DPrintFixture* fixture, Device* device) {
     // Set up program and command queue
     constexpr CoreCoord core = {0, 0}; // Print on first core only
-    Program program = Program();
+    auto program = CreateProgram();
 
     // Create a CB for testing TSLICE, dimensions are 32x32 bfloat16s
     constexpr uint32_t src0_cb_index = CB::c_in0;

230470239cf4v7r09237m/tt-metal-newci

tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_bi_tunnel.cpp
@@ -8,6 +8,7 @@
 #include "tt_metal/impl/dispatch/cq_commands.hpp"
 #include "tt_metal/hostdevcommon/common_runtime_address_map.h"
 #include "tt_metal/impl/dispatch/kernels/packet_queue_ctrl.hpp"
+#include "tt_metal/impl/program/program_pool.hpp"
 #include "kernels/traffic_gen_test.hpp"
 #include "tt_metal/impl/device/device.hpp"
 
@@ -173,8 +174,8 @@ int main(int argc, char **argv) {
         //std::cout<<"Left Tunneler = "<<tunneler_logical_core.str()<<std::endl;
         //std::cout<<"Right Tunneler = "<<r_tunneler_logical_core.str()<<std::endl;
 
-        tt_metal::Program program = tt_metal::CreateProgram();
-        tt_metal::Program program_r = tt_metal::CreateProgram();
+        auto program = tt_metal::CreateScopedProgram();
+        auto program_r = tt_metal::CreateScopedProgram();
 
         CoreCoord mux_core = {mux_x, mux_y};
         CoreCoord mux_phys_core = device->worker_core_from_logical_core(mux_core);
@@ -510,11 +511,14 @@ int main(int argc, char **argv) {
 
         log_info(LogTest, "Starting test...");
 
+        auto program_ptr = tt::tt_metal::ProgramPool::instance().get_program(program);
+        auto program_r_ptr = tt::tt_metal::ProgramPool::instance().get_program(program_r);
+
         auto start = std::chrono::system_clock::now();
-        tt_metal::detail::LaunchProgram(device, program, false);
-        tt_metal::detail::LaunchProgram(device_r, program_r, false);
-        tt_metal::detail::WaitProgramDone(device, program);
-        tt_metal::detail::WaitProgramDone(device_r, program_r);
+        tt_metal::detail::LaunchProgram(device, *program_ptr, false);
+        tt_metal::detail::LaunchProgram(device_r, *program_r_ptr, false);
+        tt_metal::detail::WaitProgramDone(device, *program_ptr);
+        tt_metal::detail::WaitProgramDone(device_r, *program_r_ptr);
         auto end = std::chrono::system_clock::now();
 
         std::chrono::duration<double> elapsed_seconds = (end-start);

tests/tt_metal/tt_metal/test_matmul_multi_core_single_dram.cpp
@@ -3,14 +3,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <algorithm>
-#include <functional>
-#include <random>
 
 #include "tt_metal/host_api.hpp"
 #include "tt_metal/detail/tt_metal.hpp"
 #include "common/bfloat16.hpp"
 #include "tt_metal/test_utils/deprecated/tensor.hpp"
 #include "test_tiles.hpp"
+#include "tt_metal/impl/program/program_pool.hpp"
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // TODO: explain what test does
@@ -152,8 +151,8 @@ std::vector<bfloat16> select_columns(std::vector<bfloat16> data, int M, int K, i
     return result;
 }
 
-std::tuple<tt_metal::Program, tt_metal::KernelHandle , tt_metal::KernelHandle> create_program(tt_metal::Device *device, int num_cores_r, int num_cores_c, int per_core_M, int per_core_N, int K, int in0_block_w, int out_subblock_h, int out_subblock_w) {
-    tt_metal::Program program = tt_metal::CreateProgram();
+std::tuple<tt_metal::ScopedProgramHandle, tt_metal::KernelHandle , tt_metal::KernelHandle> create_program(tt_metal::Device *device, int num_cores_r, int num_cores_c, int per_core_M, int per_core_N, int K, int in0_block_w, int out_subblock_h, int out_subblock_w) {
+    auto program = tt_metal::CreateScopedProgram();
     uint32_t single_tile_size = 2 * 1024;
     uint32_t in0_block_tiles = per_core_M * in0_block_w;
     uint32_t in0_CB_size = in0_block_tiles * 2 * single_tile_size; // double buffer
@@ -249,7 +248,7 @@ std::tuple<tt_metal::Program, tt_metal::KernelHandle , tt_metal::KernelHandle> c
         tt_metal::ComputeConfig{.compile_args = compute_kernel_args}
     );
 
-    return {std::move(program), mm_reader_kernel, unary_writer_kernel};
+    return {program, mm_reader_kernel, unary_writer_kernel};
 }
 
 std::vector<bfloat16> get_row_slice(std::vector<bfloat16> data, int total_row_slices, int row_slice_index, int rows, int cols) {
@@ -395,7 +394,9 @@ int main(int argc, char **argv) {
 
         log_info(LogTest, "Running Matmul {} core test", num_cores_c * num_cores_r);
 
-        tt_metal::detail::LaunchProgram(device, program);
+        auto* program_ptr = tt::tt_metal::ProgramPool::instance().get_program(program);
+        tt_metal::detail::LaunchProgram(device, *program_ptr);
+
         log_info(LogTest, "Matmul test done");
         log_info(LogTest, "Gathering data back from dram and checking against golden");
         for(int i = 0; i < num_cores_r; i++) {

tests/tt_metal/tt_metal/unit_tests/basic/device.cpp
@@ -4,18 +4,14 @@
 
 #include <gtest/gtest.h>
 
-#include <algorithm>
-#include <functional>
-#include <random>
-
 #include "tests/tt_metal/tt_metal/unit_tests/common/basic_fixture.hpp"
 #include "tests/tt_metal/tt_metal/unit_tests/common/device_fixture.hpp"
 #include "tt_metal/detail/tt_metal.hpp"
 #include "tt_metal/host_api.hpp"
 #include "tt_metal/hostdevcommon/common_runtime_address_map.h"  // FIXME: Should remove dependency on this
 #include "tt_metal/test_utils/env_vars.hpp"
-#include "tt_metal/test_utils/print_helpers.hpp"
 #include "tt_metal/test_utils/stimulus.hpp"
+#include "tt_metal/impl/program/program_pool.hpp"
 
 using namespace tt;
 using namespace tt::test_utils;
@@ -230,7 +226,7 @@ TEST_F(DeviceFixture, ValidateKernelDoesNotTargetHarvestedCores) {
             tt_metal::detail::WriteToDeviceL1(this->devices_.at(id), logical_core, write_address, host_input);
         }
 
-        tt_metal::Program program = tt_metal::CreateProgram();
+        auto program = tt_metal::CreateScopedProgram();
         string kernel_name = "tests/tt_metal/tt_metal/test_kernels/misc/ping_legal_l1s.cpp";
         CoreCoord logical_target_core(0, 0);
         uint32_t intermediate_l1_addr = devices_.at(id)->get_base_allocator_addr(HalMemType::L1);
@@ -244,7 +240,8 @@ TEST_F(DeviceFixture, ValidateKernelDoesNotTargetHarvestedCores) {
                 .noc = tt_metal::NOC::NOC_0,
                 .compile_args = {l1_address, intermediate_l1_addr, size_bytes}});
 
-        tt_metal::detail::LaunchProgram(this->devices_.at(id), program);
+        auto* program_ptr = tt::tt_metal::ProgramPool::instance().get_program(program);
+        tt_metal::detail::LaunchProgram(this->devices_.at(id), *program_ptr);
 
         std::vector<uint32_t> output;
         for (uint32_t bank_id = 0; bank_id < num_l1_banks; bank_id++) {
@@ -282,7 +279,7 @@ TEST_F(DeviceFixture, TestDeviceToHostMemChannelAssignment) {
 
 // Test to ensure writing from 16B aligned L1 address to 16B aligned PCIe address works
 TEST_F(DeviceFixture, TestL1ToPCIeAt16BAlignedAddress) {
-    tt_metal::Program program = tt_metal::CreateProgram();
+    auto program = tt_metal::CreateScopedProgram();
     Device *device = this->devices_.at(0);
     EXPECT_TRUE(device->is_mmio_capable());
     CoreCoord logical_core(0, 0);
@@ -310,7 +307,8 @@ TEST_F(DeviceFixture, TestL1ToPCIeAt16BAlignedAddress) {
         }
     );
 
-    tt_metal::detail::LaunchProgram(device, program);
+    auto* program_ptr = tt::tt_metal::ProgramPool::instance().get_program(program);
+    tt_metal::detail::LaunchProgram(device, *program_ptr);
 
     std::vector<uint32_t> result(size_bytes/sizeof(uint32_t));
     chip_id_t mmio_device_id = tt::Cluster::instance().get_associated_mmio_device(device->id());

tests/tt_metal/tt_metal/unit_tests_common/dprint/test_eth_cores.cpp
@@ -44,7 +44,7 @@ static void RunTest(DPrintFixture* fixture, Device* device, bool active) {
     }
     for (const auto& core : test_cores) {
         // Set up program and command queue
-        Program program = Program();
+        auto program = CreateScopedProgram();
 
         // Create the kernel
         // TODO: When #6424 is fixed combine these kernels again.

tests/tt_metal/tt_metal/unit_tests_common/dprint/test_mute_device.cpp
@@ -43,7 +43,7 @@ SLICE:
 static void RunTest(DPrintFixture* fixture, Device* device) {
     // Set up program and command queue
     constexpr CoreCoord core = {0, 0}; // Print on first core only
-    Program program = Program();
+    auto program = CreateScopedProgram();
 
     // Create a CB for testing TSLICE, dimensions are 32x32 bfloat16s
     constexpr uint32_t src0_cb_index = CB::c_in0;